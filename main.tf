082dde9e5d32d50fe927cc8941a7ac02511aae99
fuzzycom <fuzzycom@gmail.com>
2022-05-27 16:57:25 +0900

    Add ALB and so on

--- main.tf
+++ main.tf
@@ -2,19 +2,83 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0c55b159cbfafe1f0"
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8080
+}
+resource "aws_launch_configuration" "example" {
+  image_id      = "0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
                 #!bin/bash
                 echo "Hello, World" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
-    tags = {
-      "Name" = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+# resource "aws_instance" "example" {
+#   ami = "ami-0c55b159cbfafe1f0"
+#   instance_type = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.instance.id]
+
+#   user_data = <<-EOF
+#                 #!bin/bash
+#                 echo "Hello, World" > index.html
+#                 nohup busybox httpd -f -p 8080 &
+#                 EOF
+#     tags = {
+#       "Name" = "terraform-example"
+#     }
+# }
+
+resource "aws_lb" "example" {
+  name = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnet_ids.default.ids
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed-response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
     }
+  }
 }
 
 resource "aws_security_group" "instance" {
@@ -25,5 +89,23 @@ resource "aws_security_group" "instance" {
     to_port = 8080
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress = {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
   }
 }
